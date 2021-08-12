cebe9ae764c714e86dd144daa9d51ed5c2ead837
Esmael-a <aesmael@miu.edu>
2021-08-12 00:12:36 -0500

    last commit

--- src/main/java/com/example/airline_reservation/AirlineReservationApplication.java
+++ src/main/java/com/example/airline_reservation/AirlineReservationApplication.java
@@ -1,12 +1,20 @@
 package com.example.airline_reservation;
 
+import com.example.airline_reservation.AOP.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+//@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class AirlineReservationApplication {
 
     public static void main(String[] args) {
+
+//        ApplicationContext context = new AnnotationConfigApplicationContext(config.class);
+
 
         SpringApplication.run(AirlineReservationApplication.class, args);
 
